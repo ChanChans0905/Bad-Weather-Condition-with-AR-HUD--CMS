0fab82d88ab60d2575041ec7def98f84c5e44617
Chan <chanchans0905@gmail.com>
2023-08-25 21:48:59 +0900

    2023.08.25

---
+++ Chan/New/AccidentPathWay_SuddenStartFrom3rdLane.cs
@@ -0,0 +1,28 @@
+using PathCreation;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccidentPathWay_SuddenStartFrom3rdLane : MonoBehaviour
+{
+    [SerializeField] ElectricScooter ES;
+    public PathCreator pathCreator;
+    public GameObject Car;
+
+    void Update()
+    {
+        NormalDrive();
+    }
+
+    void NormalDrive()
+    {
+        if (ES.ScooterExitZone)
+        {
+            ES.distanceTravelled += Time.deltaTime * 5f;
+        }
+        else
+            ES.distanceTravelled = 0;
+        Car.transform.position = pathCreator.path.GetPointAtDistance(ES.distanceTravelled);
+        Car.transform.rotation = pathCreator.path.GetRotationAtDistance(ES.distanceTravelled);
+    }
+}
---
+++ Chan/New/AccidentPathWay_TurnRightThenStop.cs
@@ -0,0 +1,28 @@
+using PathCreation;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccidentPathWay_TurnRightThenStop : MonoBehaviour
+{
+    [SerializeField] ElectricScooter ES;
+    public PathCreator pathCreator;
+    float StoppingTimer;
+    public GameObject Car;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ES.ScooterEnterAccidentCollidor) NormalDrive();
+    }
+
+    void NormalDrive()
+    {
+        StoppingTimer += Time.deltaTime;
+        if (!(StoppingTimer >= 3 && StoppingTimer <= 6))
+            ES.distanceTravelled += Time.deltaTime * 20f;
+
+        Car.transform.position = pathCreator.path.GetPointAtDistance(ES.distanceTravelled);
+        Car.transform.rotation = pathCreator.path.GetRotationAtDistance(ES.distanceTravelled);
+    }
+}
