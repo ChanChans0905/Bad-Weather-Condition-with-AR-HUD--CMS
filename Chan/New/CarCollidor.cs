3750ee640abce214925399c607e5877011e21e88
Chan <chanchans0905@gmail.com>
2023-09-13 18:35:49 +0900

    2023.09.13

---
+++ Chan/New/CarCollidor.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static OVRPlugin;
+
+public class CarCollidor : MonoBehaviour
+{
+    Vector3 StartPos;
+    Quaternion StartRot;
+
+    void Start()
+    {
+        StartPos = transform.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Car" || other.tag == "EndPoint")
+        {
+            transform.position = StartPos;
+            transform.rotation = StartRot;
+        }
+
+    }
+}
